aa1c2969973ec7b921fe80fe3bab00d3ae8db289
Alifwrdhaaa <alifwrdha12@gmail.com>
2023-11-20 01:44:02 +0700

    Pesan commit Anda di sini

--- app.py
+++ app.py
@@ -84,6 +84,7 @@ def sign_in():
             "exp": datetime.utcnow() + timedelta(seconds=60 * 60 * 24),
         }
         token = jwt.encode(payload, SECRET_KEY, algorithm="HS256")
+        
 
         return jsonify(
             {
